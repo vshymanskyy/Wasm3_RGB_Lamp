dfb8a80f2e14db15719bee9458662454ca9d50f0
MaxGraey <maxgraey@gmail.com>
2020-01-30 13:16:14 +0200

    refactor hue2Rgb

--- assemblyscript/app.ts
+++ assemblyscript/app.ts
@@ -29,9 +29,9 @@ function clamp(x: i32): i32 {
 @inline
 function hue2Rgb(hue: i32): u32 {
   hue *= 6 << 8;
-  let r = abs(hue - (3 << 16)) - (1 << 16);
-  let g = (2 << 16) - abs(hue - (2 << 16));
-  let b = (2 << 16) - abs(hue - (4 << 16));
+  let r = -(1 << 16) + abs(hue - (3 << 16));
+  let g =  (2 << 16) - abs(hue - (2 << 16));
+  let b =  (2 << 16) - abs(hue - (4 << 16));
   r = clamp(r >> 8);
   g = clamp(g >> 8);
   b = clamp(b >> 8);
